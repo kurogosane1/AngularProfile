54a5a023d3b12535acb7c04006a0d9da8db76ba4
kurogosane1 <magnite@hotmail.com>
2017-10-25 22:22:43 -0500

    updated the file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { IntroComponent } from './intro/intro.component';
+import { SkillsComponent } from './skills/skills.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'admin',
+    component: AdminComponent
+  }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    IntroComponent,
+    SkillsComponent,
+    AboutmeComponent,
+    PortfolioComponent,
+    FooterComponent,
+    HomeComponent,
+    AdminComponent,
+    ResumeComponent,
+    ContactMeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
